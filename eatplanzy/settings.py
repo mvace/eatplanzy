203935f27ae1846430c47b87244391cc7522424a
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2023-09-28 11:42:10 +0200

    Updated the index page

--- eatplanzy/settings.py
+++ eatplanzy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "recipe",
+    "main_app",
 ]
 
 MIDDLEWARE = [
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from recipe.models import Recipe
+from django.db.models import F, Func, Value
+import random
+
 
 # Create your views here.
+def index(request):
+    # Get all the IDs from the Recipe model
+    recipe_ids = list(
+        Recipe.objects.exclude(maincategory__in=["inspiration", "baking"]).values_list(
+            "id", flat=True
+        )
+    )
+
+    # Get 5 random IDs
+    random_recipe_ids = random.sample(recipe_ids, 12)
+
+    # Fetch the Recipe objects with the selected random IDs
+    random_recipes = Recipe.objects.filter(id__in=random_recipe_ids)
+
+    return render(request, "main_app/index.html", {"recipes": random_recipes})
