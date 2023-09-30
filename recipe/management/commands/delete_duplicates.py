20165c58dd9dc1241bfbce22258151fc913892ae
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2023-09-30 13:41:17 +0200

    Added tags and options to share to recipe view. Delete duplicates based on name_slug field

--- recipe/management/commands/delete_duplicates.py
+++ recipe/management/commands/delete_duplicates.py
@@ -4,26 +4,28 @@ from django.db.models import Count
 
 
 class Command(BaseCommand):
-    help = "Remove duplicate records from Recipe model"
+    help = "Remove duplicate records from Recipe model based on name_slug"
 
     def handle(self, *args, **kwargs):
-        duplicate_urls = (
-            Recipe.objects.values("url")
-            .annotate(count=Count("url"))
+        duplicate_name_slugs = (
+            Recipe.objects.values("name_slug")
+            .annotate(count=Count("name_slug"))
             .filter(count__gt=1)
         )
 
-        for url_info in duplicate_urls:
-            url = url_info["url"]
-            count = url_info["count"]
-            self.stdout.write(f"Duplicate URL: {url}, Count: {count}")
+        for name_slug_info in duplicate_name_slugs:
+            name_slug = name_slug_info["name_slug"]
+            count = name_slug_info["count"]
+            self.stdout.write(f"Duplicate name_slug: {name_slug}, Count: {count}")
 
-            # Retrieve all duplicates for the current URL
-            duplicates_to_delete = Recipe.objects.filter(url=url)
+            # Retrieve all duplicates for the current name_slug
+            duplicates_to_delete = Recipe.objects.filter(name_slug=name_slug)
 
             # Keep the first record and delete the rest
             keep_record = duplicates_to_delete.first()
             duplicates_to_delete.exclude(pk=keep_record.pk).delete()
-            print("Duplicate deleted")
+            print("Duplicates deleted")
 
-        self.stdout.write(self.style.SUCCESS("Duplicates removed successfully"))
+        self.stdout.write(
+            self.style.SUCCESS("Duplicates based on name_slug removed successfully")
+        )
