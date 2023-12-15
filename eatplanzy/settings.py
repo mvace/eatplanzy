26b2527ea30f88a0883066a8d73c9ad81143324f
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2023-12-15 07:32:47 +0100

    Commit before deployement

--- eatplanzy/settings.py
+++ eatplanzy/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&=&)y2zd)u3(oy2opfnqn-&*f%4q+a^l1xv**bsf*u-@hj+_)e"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
