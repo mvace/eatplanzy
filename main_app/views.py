3001c92f37967ed705fad75768b553640cf96b33
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2024-03-25 08:00:22 +0100

    Fixed index comments

--- main_app/views.py
+++ main_app/views.py
@@ -6,14 +6,13 @@ import random
 
 # Create your views here.
 def index(request):
-    # Get all the IDs from the Recipe model
     recipe_ids = list(
         Recipe.objects.exclude(maincategory__in=["inspiration", "baking"]).values_list(
             "id", flat=True
         )
     )
 
-    # Get 5 random IDs
+    # Get random IDs
     random_recipe_ids = random.sample(recipe_ids, 120)
 
     # Fetch the Recipe objects with the selected random IDs
