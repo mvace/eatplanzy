50e727f81bc182d331e60ccf6da2964d8e7d4290
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2024-08-16 17:14:11 +0200

    env

--- eatplanzy/settings.py
+++ eatplanzy/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import os
 import dj_database_url
+import environ
+
+env = environ.Env(DEBUG=(bool, False))environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,12 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&=&)y2zd)u3(oy2opfnqn-&*f%4q+a^l1xv**bsf*u-@hj+_)e"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
